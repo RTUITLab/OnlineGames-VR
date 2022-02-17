166ef498319775072cc9ff5c36bf9ec3ebd5c538
Eloren1 <elorenn@bk.ru>
2022-02-17 13:50:08 +0300

    Fix rules prefab twitching

---
+++ Assets/Scripts/GameModes/Checkers/Rules.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rules : MonoBehaviour
+{
+    private void Start()
+    {
+        PhotonNetwork.AllocateViewID(GetComponent<PhotonView>());
+    }
+
+}
